80d38c983ba8ba7809c459dd4af628f80290bb16
Nick Nguyen <ptnguyen1901@gmail.com>
2020-03-02 16:33:27 -0500

    Adding View Controllers

---
+++ Food-Truck-TrackR/Food-Truck-TrackR/SignInViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignInViewController.swift
+//  Food-Truck-TrackR
+//
+//  Created by Nick Nguyen on 3/2/20.
+//  Copyright © 2020 Michael. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+   
+    }
+    
+
+    
+
+}
---
+++ Food-Truck-TrackR/Food-Truck-TrackR/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  Food-Truck-TrackR
+//
+//  Created by Nick Nguyen on 3/2/20.
+//  Copyright © 2020 Michael. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
