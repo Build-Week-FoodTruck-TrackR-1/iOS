e4c44d12e122c9fac6ec06967562c9c2fd6f6b42
Kerby Jean <kerby.jean@hotmail.fr>
2020-03-03 00:56:03 -0500

    Add Trucker storyboard

--- Food-Truck-TrackR/Food-Truck-TrackR/ViewController/LoginViewController.swift
+++ Food-Truck-TrackR/Food-Truck-TrackR/ViewController/LoginViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        DispatchQueue.main.async {
-            
-        }
+
     }
+    
+    
     
     
 
