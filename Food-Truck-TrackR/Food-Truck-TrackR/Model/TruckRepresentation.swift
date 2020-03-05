13499e6b4f912cb6c7a714e6b7f6d3dc61b06b34
Michael <maybemichael@icloud.com>
2020-03-04 21:29:34 -0800

    Networking and 90% of Core Data Complete

--- Food-Truck-TrackR/Food-Truck-TrackR/Model/TruckRepresentation.swift
+++ Food-Truck-TrackR/Food-Truck-TrackR/Model/TruckRepresentation.swift
@@ -14,13 +14,5 @@ struct TruckRepresentation: Codable {
     var image: String
     var cuisineType: String
     var physicalAddress: String
-    
-    enum TruckKeys: String, CodingKey {
-        case id
-        case name
-        case image
-        case cuisingType = "cuising_type"
-        case physicalAddress = "physical_address"
-    }
 }
 
--- Food-Truck-TrackR/Food-Truck-TrackR/Representation + Ext/MenuItem + Ext.swift
+++ Food-Truck-TrackR/Food-Truck-TrackR/Representation + Ext/MenuItem + Ext.swift
@@ -9,35 +9,35 @@
 import Foundation
 import CoreData
 
-extension MenuItem {
-    
-    
-    var jsonMenuItem: JSONMenuItem? {
-        guard let itemPhotos = itemPhotos as? [URL], let customerRatings = customerRatings as? [Int64] else { return nil }
-    
-        return JSONMenuItem(itemPrice: itemPrice,
-                            itemPhotos:itemPhotos,
-                            itemName: itemName,
-                            itemDescription: itemDescription,
-                            customerRatings: customerRatings,
-                            customerRatingAvg: customerRatingAvg)
-        
-    }
-    
-    
-    
-    
-     // This creates a new managed object from raw data
-    @discardableResult convenience init(itemPrice: Double, itemPhotos: [URL], itemName: String, itemDescription: String,customerRatings: [Int64], customerRatingAvg: Int64, context: NSManagedObjectContext = CoreDataStack.shared.mainContext ) {
-        
-        self.init(context:context)
-        self.itemPrice = itemPrice
-        self.itemPhotos = itemPhotos as NSObject
-        self.itemName = itemName
-        self.itemDescription = itemDescription
-        self.customerRatings = customerRatings as NSObject
-        self.customerRatingAvg = customerRatingAvg
-    }
+//extension MenuItem {
+//    
+//    
+//    var jsonMenuItem: JSONMenuItem? {
+//        guard let itemPhotos = itemPhotos as? [URL], let customerRatings = customerRatings as? [Int64] else { return nil }
+//    
+//        return JSONMenuItem(itemPrice: itemPrice,
+//                            itemPhotos:itemPhotos,
+//                            itemName: itemName,
+//                            itemDescription: itemDescription,
+//                            customerRatings: customerRatings,
+//                            customerRatingAvg: customerRatingAvg)
+//        
+//    }
+//    
+//    
+//    
+//    
+//     // This creates a new managed object from raw data
+//    @discardableResult convenience init(itemPrice: Double, itemPhotos: [URL], itemName: String, itemDescription: String,customerRatings: [Int64], customerRatingAvg: Int64, context: NSManagedObjectContext = CoreDataStack.shared.mainContext ) {
+//        
+//        self.init(context:context)
+//        self.itemPrice = itemPrice
+//        self.itemPhotos = itemPhotos as NSObject
+//        self.itemName = itemName
+//        self.itemDescription = itemDescription
+//        self.customerRatings = customerRatings as NSObject
+//        self.customerRatingAvg = customerRatingAvg
+//    }
     
     
     
@@ -45,21 +45,21 @@ extension MenuItem {
     // This creates a managed object from  JSON  (which comes from Firebase)
     
     
-    @discardableResult convenience init?(jsonMenuItem: JSONMenuItem, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
-        
-        guard let itemPrice = jsonMenuItem.itemPrice ,
-            let itemName = jsonMenuItem.itemName,
-            let itemDescription = jsonMenuItem.itemDescription,
-            let customerRatingAvg = jsonMenuItem.customerRatingAvg else { return nil }
-        
-        
-        self.init(itemPrice:itemPrice,
-                  itemPhotos:jsonMenuItem.itemPhotos,
-                  itemName:  itemName,
-                  itemDescription: itemDescription,
-                  customerRatings: jsonMenuItem.customerRatings,
-                  customerRatingAvg:customerRatingAvg,
-                  context:context)
-        
-    }
-}
+//    @discardableResult convenience init?(jsonMenuItem: JSONMenuItem, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+//        
+//        guard let itemPrice = jsonMenuItem.itemPrice ,
+//            let itemName = jsonMenuItem.itemName,
+//            let itemDescription = jsonMenuItem.itemDescription,
+//            let customerRatingAvg = jsonMenuItem.customerRatingAvg else { return nil }
+//        
+//        
+//        self.init(itemPrice:itemPrice,
+//                  itemPhotos:jsonMenuItem.itemPhotos,
+//                  itemName:  itemName,
+//                  itemDescription: itemDescription,
+//                  customerRatings: jsonMenuItem.customerRatings,
+//                  customerRatingAvg:customerRatingAvg,
+//                  context:context)
+//        
+//    }
+//}
